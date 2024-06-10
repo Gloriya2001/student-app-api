60a2d36f554e40b8cb7d795c6d3af0050be94cca
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-10 23:16:27 +0530

    more fields addes in schema

--- models/student.js
+++ models/student.js
@@ -2,8 +2,14 @@ const mongoose = require("mongoose")
 const schema = mongoose.Schema(
     {
         "name": String,
-        "rollNo": String
-
+        "rollNo": String,
+        "admNo":String,
+        "parentName":String,
+        "collegeName":String,
+        "dob":String,
+        "emailId":String,
+        "password":String,
+        "confirmPassword":String
 
     }
 )
